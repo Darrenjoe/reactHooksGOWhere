f4759f3414201157c5eba85e1eb93891c6b7445a
Darrenjoe <491361994@qq.com>
2019-12-16 20:04:49 +0800

    redux

--- train-ticket/src/App.jsx
+++ train-ticket/src/App.jsx
@@ -1,12 +1,5 @@
 import React, { Component, PureComponent, useState } from "react";
 
-class Counter extends PureComponent {
-  render() {
-    const { props } = this;
-    return <h1>{props.count}</h1>;
-  }
-}
-
 function App() {
   const [count, setCount] = useState(0);
   return (
