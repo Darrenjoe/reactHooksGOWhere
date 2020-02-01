4e63b64116b51a904c0b424ef287de19356f1834
Darrenjoe <491361994@qq.com>
2020-02-01 17:45:41 +0800

    todolist

--- train-ticket/src/App.jsx
+++ train-ticket/src/App.jsx
@@ -1,126 +1,20 @@
-import React, {
-  Component,
-  useEffect,
-  useMemo,
-  useRef,
-  useCallback,
-  useState
-} from "react";
+import React, { useEffect, useState, useMemo, useRef } from "react";
 import "./App.css";
 
-let idSeq = Date.now();
-
-function Control(props) {
-  const { addTodo } = props;
-  const inputRef = useRef();
-
-  const onSubmit = e => {
-    e.preventDefault();
-
-    const newText = inputRef.current.value.trim();
-
-    if (newText.length === 0) {
-      return;
-    }
-
-    addTodo({
-      id: ++idSeq,
-      text: newText,
-      complete: false
-    });
-
-    inputRef.current.value = "";
-  };
-
-  return (
-    <div className="control">
-      <h1>Todos list</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          ref={inputRef}
-          className="new-todo"
-          placeholder="What needs to be done"
-        ></input>
-      </form>
-    </div>
-  );
+function Control() {
+  return <div>Control</div>;
 }
 
-function TodoItem(props) {
-  const {
-    todos: { id, text, complete },
-    toggleTodo,
-    removeTodo
-  } = props;
-
-  const onChange = () => {
-    toggleTodo(id);
-  };
-  const onRemove = () => {
-    removeTodo(id);
-  };
-
-  return (
-    <li className="todo-item">
-      <input type="checkbox" onChange={onChange} checked={complete} />
-      <label className={complete ? "complete" : ""}>{text}</label>
-      <button onClick={onRemove}>&#xd7;</button>
-    </li>
-  );
-}
-
-function Todos(props) {
-  const { todos, toggleTodo, removeTodo } = props;
-
-  return (
-    <ul>
-      {todos.map(todo => {
-        return (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            toggleTodo={toggleTodo}
-            removeTodo={removeTodo}
-          />
-        );
-      })}
-    </ul>
-  );
+function Todos() {
+  return <div>todos</div>;
 }
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
-
-  const addTodo = useCallback(todo => {
-    setTodos(todos => [...todos, todo]);
-  }, []);
-
-  const removeTodo = useCallback(id => {
-    setTodos(todos =>
-      todos.filter(todo => {
-        return todo.id !== id;
-      })
-    );
-  }, []);
-
-  const toggleTodo = useCallback(id => {
-    setTodos(todos =>
-      todos.map(todo => {
-        return todo.id === id
-          ? {
-              ...todo,
-              complete: !todo.complete
-            }
-          : todo;
-      })
-    );
-  }, []);
-
   return (
     <div className="todo-list">
-      <Control addTodo={addTodo} />
-      <Todos removeTodo={removeTodo} toggleTodo={toggleTodo} todos={todos} />
+      <Control />
+      <Todos />
     </div>
   );
 }
