2f13680a35a7237b10ef7602f806425fce079a4b
Darrenjoe <491361994@qq.com>
2020-02-07 21:39:30 +0800

    station

--- train-ticket/src/index/Journey.jsx
+++ train-ticket/src/index/Journey.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import switchImg from "./imgs/switch.svg";
 import "./Journey.css";
 
 function Journey(props) {
-  return <div></div>;
+  const { from, to, exchangeFromTo, showCtySelector } = props;
+
+  return (
+    <div className="journey">
+      <div className="journey-station" onClick={() => showCtySelector(true)}>
+        <input
+          type="text"
+          readOnly
+          name="from"
+          value={from}
+          className="journey-input journey-from"
+        />
+      </div>
+      <div className="journey-switch" onClick={() => exchangeFromTo()}>
+        <img src={switchImg} width="70" height="40" alt="switch" />
+      </div>
+      <div className="journey-station" onClick={() => showCtySelector(false)}>
+        <input
+          type="text"
+          readOnly
+          name="to"
+          value={to}
+          className="journey-input journey-to"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Journey;
