c4d19810d6a57c51fc0e5b78a660f11153e7c0cc
Darrenjoe <491361994@qq.com>
2020-02-12 18:25:47 +0800

    Time

--- train-ticket/src/index/DepartDate.jsx
+++ train-ticket/src/index/DepartDate.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { h0 } from "../common/fp";
+import dayjs from "dayjs";
 import "./DepartDate.css";
 
 function DepartDate(props) {
-  return <div></div>;
+  const { time, onClick } = props;
+
+  const departDateString = useMemo(() => {
+    return dayjs(time).format("YYYY-MM-DD");
+  }, [time]);
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 }
 
 export default DepartDate;
