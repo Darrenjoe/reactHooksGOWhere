98fc3cccb7e493750e76d6211da1d9c57d2a3b6b
Darrenjoe <491361994@qq.com>
2020-03-03 22:58:03 +0800

    schedule

--- train-ticket/src/ticket/Schedule.jsx
+++ train-ticket/src/ticket/Schedule.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import classnames from "classnames";
 import URI from "urijs";
 import leftPad from "left-pad";
+import dayjs from "dayjs";
 
 import "./Schedule.css";
 
@@ -68,9 +69,93 @@ function Schedule(props) {
 
   const [scheduleList, setScheduleList] = useState([]);
 
+  useEffect(() => {
+    const url = new URI("/rest/schedule")
+      .setSearch("trainNumber", trainNumber)
+      .setSearch("departStation", departStation)
+      .setSearch("arriveStation", arriveStation)
+      .setSearch("date", dayjs(date).format("YYYY-MM-DD"))
+      .toString();
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let departRow;
+        let arriveRow;
+
+        for (let i = 0; i < data.length; i++) {
+          if (!departRow) {
+            if (data[i].station === departStation) {
+              departRow = Object.assign(data[i], {
+                beforeDepartStation: false,
+                isDepartStation: true,
+                afterArriveStation: false,
+                isArriveStation: false
+              });
+            } else {
+              Object.assign(data[i], {
+                beforeDepartStation: true,
+                isDepartStation: false,
+                afterArriveStation: false,
+                isArriveStation: false
+              });
+            }
+          } else if (!arriveRow) {
+            if (data[i].station === arriveStation) {
+              arriveRow = Object.assign(data[i], {
+                beforeDepartStation: false,
+                isDepartStation: false,
+                afterArriveStation: false,
+                isArriveStation: true
+              });
+            } else {
+              Object.assign(data[i], {
+                beforeDepartStation: false,
+                isDepartStation: false,
+                afterArriveStation: false,
+                isArriveStation: false
+              });
+            }
+          } else {
+            Object.assign(data[i], {
+              beforeDepartStation: false,
+              isDepartStation: false,
+              afterArriveStation: true,
+              isArriveStation: false
+            });
+          }
+
+          Object.assign(data[i], {
+            isStartStation: i === 0,
+            isEndStation: i === data.length - 1
+          });
+        }
+
+        setScheduleList(data);
+      });
+  }, [date, trainNumber, departStation, arriveStation]);
+
   return (
     <div className="schedule">
-      <div>schedule</div>
+      <div className="dialog">
+        <h1>列车时刻表</h1>
+        <div className="head">
+          <span className="station">车站</span>
+          <span className="deptime">到达</span>
+          <span className="arrtime">发车</span>
+          <span className="stoptime">停留</span>
+        </div>
+        <ul>
+          {scheduleList.map((schedule, index) => {
+            return (
+              <ScheduleRow
+                key={schedule.station}
+                index={index + 1}
+                {...schedule}
+              />
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
