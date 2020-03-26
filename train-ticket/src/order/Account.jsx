15cb3935891b2bf488235faec35d9884c8779a28
Darrenjoe <491361994@qq.com>
2020-03-26 21:48:40 +0800

    order

--- train-ticket/src/order/Account.jsx
+++ train-ticket/src/order/Account.jsx
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import "./Account.css";
 
 const Account = memo(function Account(props) {
+  const { price = 0, length } = props;
+
   return (
     <div className="account">
       <div></div>
@@ -11,6 +13,9 @@ const Account = memo(function Account(props) {
   );
 });
 
-Account.propTypes = {};
+Account.propTypes = {
+  price: PropTypes.number,
+  length: PropTypes.number.isRequired
+};
 
 export default Account;
